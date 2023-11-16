ef1574cc4b040574a1d8a4ebf836b11e55c85993
Amit <amitkumar03022004@gmail.com>
2023-11-17 04:53:13 +0530

    final restful

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const port = 8080;
 const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat");
+const methodOverride = require("method-override");
+
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
 app.use(express.static(path.join(__dirname,"public")));
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 main().then((data)=>{
   console.log(data);
 })
@@ -56,15 +59,29 @@ app.post("/chats",(req,res)=>{
   })
   res.redirect("/chats");
 })
+//edit
 app.get("/chats/:id/edit",async(req,res)=>{
   let {id} = req.params;
   let chat = await Chat.findById(id);
   res.render("edit.ejs",{chat});
 })
 
+//update
+app.put("/chats/:id", async (req,res)=>{
+  let {id} = req.params;
+  let {msg :newMsg} = req.body; // yaha par msg bala data ko newMsg me store kar raha hai
+  let updateChat = await Chat.findByIdAndUpdate(id,{msg:newMsg},{
+    runValidators:true, new:true});
 
-//edit
-
+  res.redirect("/chats");
+});
+//delete button
+app.delete("/chats/:id",async(req,res)=>{
+  let {id} = req.params;
+    let deleteChat = await Chat.findByIdAndDelete(id);
+    console.log("Deleted chat");
+    res.redirect("/chats");
+})
 app.listen(port,()=>{
   console.log(`listening to port ${port}`);
 })
