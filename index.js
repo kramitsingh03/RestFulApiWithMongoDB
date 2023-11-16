8ab8bb4d118c74a654c8244fab3e3940366ee836
Amit <amitkumar03022004@gmail.com>
2023-11-17 03:13:21 +0530

    update

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const Chat = require("./models/chat");
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
 app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({extended:true}));
 main().then((data)=>{
   console.log(data);
 })
@@ -17,6 +18,8 @@ async function main() {
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
+
+
 let chat1 = new Chat({
   from:"neha",
   to:"priya",
@@ -34,6 +37,33 @@ app.get("/chats", async (req,res)=>{
   let chats = await Chat.find();
   res.render("index.ejs",{chats});
 })
+
+app.get("/chats/new",(req,res)=>{
+  res.render("new.ejs");
+})
+app.post("/chats",(req,res)=>{
+  let {from,to,msg} = req.body;
+  let newChat = new Chat({
+    from:from,
+    to:to,
+    msg:msg,
+    created_at:new Date()
+  })
+  newChat.save().then(res=>{
+    console.log("Chat saved succesfully");
+  }).catch(err=>{
+    console.log("err");
+  })
+  res.redirect("/chats");
+})
+app.get("/chats/:id/edit",async(req,res)=>{
+  let {id} = req.params;
+  let chat = await Chat.findById(id);
+  res.render("edit.ejs",{chat});
+})
+
+
+//edit
 
 app.listen(port,()=>{
   console.log(`listening to port ${port}`);
